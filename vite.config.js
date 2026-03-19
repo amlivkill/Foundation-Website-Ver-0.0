68ade37334931ec1da456a118bd283a103381890
amlivkill <anupamrayal@yahoo.com>
2026-03-19 16:44:42 +0530

    fix(deploy): update base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Foundation-Website-Ver-0.0/',
+  base: '/',
   server: {
     proxy: {
       '/api': {
